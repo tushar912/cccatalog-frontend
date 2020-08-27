ea9b6777944a7bc4835cc5bd50b86bb368469bd4
Zack Krida <zackkrida@pm.me>
2020-08-27 09:52:09 -1000

    Move assets; use inline svg for header and footer

--- components/FooterSection.vue
+++ components/FooterSection.vue
@@ -12,15 +12,7 @@
             href="https://creativecommons.org"
             class="main-logo margin-bottom-bigger has-text-white"
           >
-            <svg
-              aria-labelledby="title"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 304 73"
-            >
-              <title id="title" lang="en">Logo</title>
-              <use href="/static/logos/cc/logomark.svg#logomark"></use>
-            </svg>
+            <IconCCLogo />
           </a>
           <address class="margin-bottom-normal">
             <span class="is-block">{{ $t('footer.address.title') }}</span>
@@ -161,21 +153,12 @@
 </template>
 
 <script>
+import IconCCLogo from '@creativecommons/vocabulary/assets/logos/cc/logomark.svg?inline'
+
 export default {
   name: 'footer-section',
+  components: {
+    IconCCLogo,
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.donate,
-.social {
-  &:hover {
-    text-decoration: none;
-  }
-}
-
-.icon {
-  vertical-align: middle;
-}
-</style>
